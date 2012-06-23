7ace504ef2c4c59ac672c2c11744773efd258fd4
zfilm <morten@z-film.no>
2012-06-23 02:25:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplApp::Application.config.secret_token = '1cfe3ce947439412464782ae3848076d433dbca3bdf15d6046544babbb4dd4372355ba51225accfb8629629729d8e7861fca64ddd2cb49ebd5dcc15d49e32464'
