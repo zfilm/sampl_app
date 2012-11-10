53fbbb4b2144554e8b0db6d8f63bd343d745a1e2
zfilm <morten@z-film.no>
2012-11-11 00:13:47 +0100

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,14 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render "new"
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
-end
+  
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,16 +12,20 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com", 
-                     password: "foobar", password_confirmation: "foobar") }
-
-    subject { @user }
+  
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
+    
+  subject { @user }
 
     it { should respond_to(:name) }
     it { should respond_to(:email) }
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
     it { should respond_to(:authenticate) }
     
     it { should be_valid }
@@ -108,5 +112,10 @@ describe User do
           end
 
           it { should_not be_valid }
+      end
+      
+      describe "remember token" do
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
       end
 end
